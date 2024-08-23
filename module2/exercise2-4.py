0b70b14e7bfc68784e7e6441e535be25cc1a3b71
liulu <Krystalanyong@gmail.com>
2024-08-23 11:58:29 +0300

    module solution for task 1

---
+++ module2/exercise2-4.py
@@ -0,0 +1,9 @@
+num1=int(input("Enter the first number: "))
+num2=int(input("Enter the second number: "))
+num3=int(input("Enter the third number: "))
+sum=num1+num2+num3
+product=num1*num2*num3
+average=sum/3
+print(f"The sum of the numbers is {sum}")
+print(f"The product of the numbers is {product}")
+print(f"The average of the numbers is {average:.2f}")
