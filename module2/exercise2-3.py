d22b9b5d719167e61e3026d160973ca15c004a93
liulu <Krystalanyong@gmail.com>
2024-08-23 11:58:18 +0300

    module solution for task 1

---
+++ module2/exercise2-3.py
@@ -0,0 +1,6 @@
+length=float(input("Enter the length: "))
+width=float(input("Enter the width: "))
+perimeter=length*2+width*2
+area=length*width
+print(f"The perimeter is {perimeter:.2f}")
+print(f"The area is {area:.2f}")
