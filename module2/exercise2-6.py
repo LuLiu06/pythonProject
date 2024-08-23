cc18592ff80b34c116a29622a90ae79264003585
liulu <Krystalanyong@gmail.com>
2024-08-23 11:58:48 +0300

    module solution for task 1

---
+++ module2/exercise2-6.py
@@ -0,0 +1,9 @@
+import random
+three_digit_code=random.randint(100,999)
+print(three_digit_code)
+num1=random.randint(1,6)
+num2=random.randint(1,6)
+num3=random.randint(1,6)
+num4=random.randint(1,6)
+four_digit_code=num1*1000+num2*100+num3*10+num4
+print(four_digit_code)
