de5279c9bc82bf00e33da09991a6bbcddd5f8937
liulu <Krystalanyong@gmail.com>
2024-08-29 10:53:25 +0300

    module solution for task 1

---
+++ module 3/exercise3-2.py
@@ -0,0 +1,9 @@
+cabin_class=input("Enter the cabin class: ").upper()
+if cabin_class=="LUX":
+    print("It's upper-deck cabin with a balcony.")
+elif cabin_class=="A":
+    print("It's above the car deck, equipped with a window.")
+elif cabin_class=="B":
+    print("It's windowless cabin above the car deck.")
+elif cabin_class=="C":
+    print("It's windowless cabin below the car deck.")
